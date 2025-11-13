cfed3997311e0d68f4da85bb5af5693c60b7334d
RUANIFCE <ruan.goncalves08@aluno.ifce.edu.br>
2025-11-12 21:27:45 -0300

    Update questao12denovembro.py

--- questao12denovembro.py
+++ questao12denovembro.py
@@ -12,3 +12,19 @@ elif(salario>=700 and salario<1500):
 elif(salario>=1500):
   salario=salario*1.05
   print("\nsalario antigo",salarioantigo ,"\naumento:5%\nvalor aumentado:",salario-salarioantigo,"\nsalario novo",salario)
+  peso = float(input("digite o peso"))
+frete=0.0
+faixa=0
+if(peso<1):
+    frete=12.5
+    faixa=1
+elif(peso>1 and peso<5):
+    frete=20.0
+    faixa=2
+elif(peso>5 and peso<15):
+    frete=35.00
+    faixa=3
+elif(peso>15):
+    frete=35.00+(3.00*(peso-15.00))
+    faixa=4
+print("peso:",peso,"\nfaixa ",faixa,"valor frete:",frete)
